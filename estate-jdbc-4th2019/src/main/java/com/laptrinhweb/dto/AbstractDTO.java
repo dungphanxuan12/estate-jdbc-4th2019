a7e8e6b081a4c2f7b326ee455a310d1b028e242f
dungphanxuan12 <dungphanxuan12@gmail.com>
2020-08-13 20:27:49 +0700

    [1.0.0]Adding bean validator

--- estate-jdbc-4th2019/src/main/java/com/laptrinhweb/dto/AbstractDTO.java
+++ estate-jdbc-4th2019/src/main/java/com/laptrinhweb/dto/AbstractDTO.java
@@ -4,9 +4,24 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * @author dungp
+ *
+ * @param <T>
+ */
+@Getter
+@Setter
 public class AbstractDTO<T> {
 
+	@NotNull(message = "id cannot be null")
 	private Long id;
+
 	private String createdBy;
 	private String modifiedBy;
 	private Timestamp createdDate;
@@ -18,102 +33,5 @@ public class AbstractDTO<T> {
 	private String action;
 	List<T> listResults = new ArrayList<T>();
 	private Long[] ids;
-	
-
-	public List<T> getListResults() {
-		return listResults;
-	}
-
-	public void setListResults(List<T> listResults) {
-		this.listResults = listResults;
-	}
-
-	public int getMaxPageItem() {
-		return maxPageItem;
-	}
-
-	public void setMaxPageItem(int maxPageItem) {
-		this.maxPageItem = maxPageItem;
-	}
-
-	public int getPage() {
-		return page;
-	}
-
-	public void setPage(int page) {
-		this.page = page;
-	}
-
-	public String getAction() {
-		return action;
-	}
-
-	public void setAction(String action) {
-		this.action = action;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
-
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-
-	public Timestamp getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Timestamp createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Timestamp getModifiedDate() {
-		return modifiedDate;
-	}
-
-	public void setModifiedDate(Timestamp modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
-
-	public Long[] getIds() {
-		return ids;
-	}
-
-	public void setIds(Long[] ids) {
-		this.ids = ids;
-	}
-
-	public int getTotalItem() {
-		return totalItem;
-	}
-
-	public void setTotalItem(int totalItem) {
-		this.totalItem = totalItem;
-	}
-
-	public int getTotalPage() {
-		return totalPage;
-	}
-
-	public void setTotalPage(int totalPage) {
-		this.totalPage = totalPage;
-	}
 
 }
